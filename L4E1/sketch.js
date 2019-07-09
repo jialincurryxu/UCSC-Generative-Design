92f0b90e2bc8c1c707a3cdea51852a51636985a7
Curry Xu <jialincurryxu@gmail.com>
2019-07-09 14:17:05 -0700

    Finished L4E1

---
+++ L4E1/sketch.js
@@ -0,0 +1,23 @@
+let w, ruleset;
+
+function setup() {
+    createCanvas(1400, 750);
+
+    w = 10;
+    ruleset = [0,1,0,1,1,0,1,0];
+    ca = new CellularAutomata(width/w, ruleset);
+}
+
+function draw() {
+    ca.draw(w);
+    ca.generate();
+
+    if(ca.generation > height / w) {
+    	for (let i = 0; i < 7; i++){
+    		ruleset[i] = Math.round(Math.random());
+    	}
+    	console.log(ruleset);
+        ca = new CellularAutomata(width/w, ruleset);
+        clear();
+    }
+}
